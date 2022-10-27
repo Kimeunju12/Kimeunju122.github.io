47c4e36c22770b2af67564684380d1c6fe65294e
Kimeunju12 <113434823+Kimeunju12@users.noreply.github.com>
2022-10-27 18:53:30 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,69 @@
+$(function () {
+    $('.top_banner_slide').slick({
+        arrows: false,
+        dots: true,
+    });
+
+    $('.top_banner i').on('click', function () {
+        $('.top_banner').slideUp(200);
+        $('.main').addClass('on')
+    });
+
+    $(window).on('scroll', function () {
+        var sct = $(window).scrollTop()
+        if (sct > 0) {
+            $('.header .bottom').addClass('on')
+        } else {
+            $('.header .bottom').removeClass('on')
+        }
+    });
+
+    $('.header .grobal').on('click', function () {
+        $('.header .link').toggleClass('on')
+    });
+
+    $('.main_slide').slick({
+        arrows: false,
+        autoplay: true,
+        pauseOnHover: false,
+    });
+
+    $('.main_slide').on('afterChange', function (e, s, c) {
+        $('.main_visual .page li').eq(c).addClass('on').siblings().removeClass('on');
+        // c= 0,1,2
+        $('.num strong').text("0" + (c + 1))
+    });
+
+    $('.page li').on('click', function () {
+        var idx = $(this).index() // 0,1,2
+        $('.main_slide').slick('slickGoTo', idx)
+    })
+
+    $('.main_visual .arrows i:first-child').on('click', function () {
+        $('.main_slide').slick('slickPrev')
+    })
+    $('.main_visual .arrows i:last-child').on('click', function () {
+        $('.main_slide').slick('slickNext')
+    });
+
+    $('.left_slider').slick({
+        arrows: false,
+        fade: true,
+        asNavFor: '.right_slider',
+    });
+
+    $('.right_slider').slick({
+        arrows: false,
+        slidesToShow: 5,
+        asNavFor: '.left_slider',
+    });
+
+    $('.main_build .wbox .right .arrows i:first-child').on('click', function () {
+        $('.left_slider').slick('slickPrev')
+    });
+    $('.main_build .wbox .right .arrows i:last-child').on('click', function () {
+        $('.left_slider').slick('slickNext')
+    });
+
+
+})
